07e27267ccf724b0de38e40667d4f58f0df67764
pyewacket <pyewacket+shsms@gggmail.me>
2021-09-16 16:28:11 -0400

    added err message to contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,7 +5,7 @@ const contactsRouter = express.Router();
 const Contact = require('../db/Schemas/contacts');
 
 contactsRouter.get('/', (req, res) => {
-    if (req.headers.enc !== process.env.REQ_TOKEN) throw err
+    if (req.headers.enc !== process.env.REQ_TOKEN) throw new Error({message: 'Try Again...'})
     Contact.find({}, (err, contacts) => {
         if (err) throw err;
         res.send(contacts)
