62a38d3096b80dd7197683cd987c6bbb5dffed45
pyewacket <pyewacket+shsms@gggmail.me>
2021-09-16 16:24:04 -0400

    added req token check to contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,6 +5,7 @@ const contactsRouter = express.Router();
 const Contact = require('../db/Schemas/contacts');
 
 contactsRouter.get('/', (req, res) => {
+    if (req.headers.enc !== process.env.REQ_TOKEN) throw err
     Contact.find({}, (err, contacts) => {
         if (err) throw err;
         res.send(contacts)
