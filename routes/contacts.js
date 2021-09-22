7c30f98027eb2758ac28bb261e62d82217595f11
pyewacket <pyewacket+shsms@gggmail.me>
2021-09-21 23:53:53 -0400

    comments

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,7 +7,7 @@ const Contact = require('../db/Schemas/contacts');
 contactsRouter.get('/', (req, res) => {
     // if (req.headers.enc !== process.env.REQ_TOKEN) {
     //     return false
-    // }
+    // } 
     
     Contact.find({}, (err, contacts) => {
         if (err) throw err;
