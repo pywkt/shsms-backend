c22c71a370fcba8f91152a232ca8e95558853299
pyewacket <pyewacket+shsms@gggmail.me>
2021-09-21 23:57:46 -0400

    re-added header check

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,9 +5,9 @@ const contactsRouter = express.Router();
 const Contact = require('../db/Schemas/contacts');
 
 contactsRouter.get('/', (req, res) => {
-    // if (req.headers.enc !== process.env.REQ_TOKEN) {
-    //     return false
-    // } 
+    if (req.headers.enc !== process.env.REQ_TOKEN) {
+        return false
+    }
     
     Contact.find({}, (err, contacts) => {
         if (err) throw err;
